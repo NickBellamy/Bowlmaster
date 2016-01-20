2a02080bdfad4a4d589157fb2be1b240780b413c
Nick Bellamy <nick.bellamy@gmail.com>
2016-01-20 15:18:03 +0000

    Add bowling ball
    Ball is launched from Start script on ball.

---
+++ Assets/Scrips/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    public float launchSpeed;
+
+    private AudioSource audioSource;
+    private Rigidbody rigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        Launch();
+	}
+
+    public void Launch()
+    {
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+}
