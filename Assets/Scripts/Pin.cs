530555a876549abc6730c96d065e475f1d15328f
Nick Bellamy <nick.bellamy@gmail.com>
2016-01-26 19:26:12 +0000

    Add a Pin remaining count to UI

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -1,18 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pin : MonoBehaviour 
+public class Pin : MonoBehaviour
 {
-    public float standingThreshold = 3f;
-	
-	public bool IsStanding()
+    public float standingThreshold = 10f;
+
+    public bool IsStanding()
     {
-        Vector3 rotationInEuler = transform.rotation.eulerAngles;
-
-        float tiltX = Mathf.Abs(rotationInEuler.x);
-        float tiltZ = Mathf.Abs(rotationInEuler.z);
-
-        if (tiltX < standingThreshold || tiltZ < standingThreshold)
+        if (Vector3.Angle(transform.up, Vector3.up) < standingThreshold)
         {
             return true;
         }
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour 
+{
+    public Text standingDisplay;
+	
+	void Update () 
+	{
+        standingDisplay.text = CountStanding().ToString();
+	}
+
+    private int CountStanding()
+    {
+        int pinCount = 0;
+        // Currently being called every frame from update.
+        // Move FindObjectsOfType to be only called once in future.
+        foreach(Pin pin in FindObjectsOfType<Pin>())
+        {
+            if (pin.IsStanding())
+            {
+                pinCount++;
+            }
+        }
+        return pinCount;
+    }
+}
