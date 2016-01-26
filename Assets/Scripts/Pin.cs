378a57c32e687214629ff2acec985fa64d25fe70
Nick Bellamy <nick.bellamy@gmail.com>
2016-01-26 14:44:54 +0000

    Add logic to infer whether pins are standing

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour 
+{
+    public float standingThreshold = 3f;
+	
+	public bool IsStanding()
+    {
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+        float tiltX = Mathf.Abs(rotationInEuler.x);
+        float tiltZ = Mathf.Abs(rotationInEuler.z);
+
+        if (tiltX < standingThreshold || tiltZ < standingThreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
