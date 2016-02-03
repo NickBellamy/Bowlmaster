a812ceabd48fd72a418e2610e7eb060d75343c04
Nick Bellamy <nick.bellamy@gmail.com>
2016-02-03 09:13:07 +0000

    Add a set of systematic tests to game logic

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -169,4 +169,24 @@ public class ActionMasterTest
         actionMaster.Bowl(5);
         Assert.AreEqual(endGame, actionMaster.Bowl(2));
     }
+    [Test]
+    public void T17_NathansTest()
+    {
+        actionMaster.Bowl(0);
+        actionMaster.Bowl(10);
+        actionMaster.Bowl(5);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(1));
+    }
+    [Test]
+    public void T18_Dondi10thFrameTurkeyTest()
+    {
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        foreach (int roll in rolls)
+        {
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
 }
