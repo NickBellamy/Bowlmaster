0ff6b1b1eb9c057bc3e033d953685bda838cf598
Nick Bellamy <nick.bellamy@gmail.com>
2016-02-07 16:06:57 +0000

    Add GameManager object
    Handles all logic decisions in the game

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,6 +4,23 @@ using System.Collections.Generic;
 
 public class ScoreMaster
 {
+    // Returns a list of cumulative scores, like a normal Scorecard.
+    public static List<int>ScoreCumulative(List<int> rolls)
+    {
+        List<int> cumulativeScores = new List<int>();
+        int runningTotal = 0;
+
+        foreach (int frameScore in ScoreFrames(rolls))
+        {
+            runningTotal += frameScore;
+            cumulativeScores.Add(runningTotal);
+        }
+
+        return cumulativeScores;
+
+    }
+
+    // Return a list od individual frame scores.
     public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>();
