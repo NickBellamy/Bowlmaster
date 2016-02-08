b380afb21cc984b6d0a7a852fa5f2fb286a91440
Nick Bellamy <nick.bellamy@gmail.com>
2016-02-08 14:18:10 +0000

    Add ScoreMaster by TDD

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class ScoreMaster
 {
     // Returns a list of cumulative scores, like a normal Scorecard.
-    public static List<int>ScoreCumulative(List<int> rolls)
+    public static List<int> ScoreCumulative(List<int> rolls)
     {
         List<int> cumulativeScores = new List<int>();
         int runningTotal = 0;
@@ -15,18 +15,39 @@ public class ScoreMaster
             runningTotal += frameScore;
             cumulativeScores.Add(runningTotal);
         }
-
         return cumulativeScores;
 
     }
 
-    // Return a list od individual frame scores.
+    // Return a list of individual frame scores.
     public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>();
 
-        // Code here
+        for (int i = 2; i <= rolls.Count; i += 2)
+        {
 
+            int frameFirstBowl = rolls[i - 2];
+            int frameSecondBowl = rolls[i - 1];
+
+            // If strike or spare
+            if (frameFirstBowl == 10 || frameFirstBowl + frameSecondBowl == 10)
+            {
+
+                // If next 2 bowls have been bowled
+                if (i < rolls.Count)
+                {
+                    frameList.Add(rolls[i - 2] + rolls[i - 1] + rolls[i]);
+                    i -= frameFirstBowl / 10;
+                }
+            }
+
+            // If all frames not full
+            else if (frameList.Count <= 9)
+            {
+                frameList.Add(frameFirstBowl + frameSecondBowl);
+            }
+        }
         return frameList;
     }
 }
