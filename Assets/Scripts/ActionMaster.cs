92f0d989b2a07befb182c65e3e7fce635d48ff06
Nick Bellamy <nick.bellamy@gmail.com>
2016-02-15 11:04:08 +0000

    Rewrite of ActionMaster
    The ActionMaster script was rewritten because it needed to become a static class to follow the designated architecture.

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -2,56 +2,64 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ActionMaster
+public static class ActionMaster
 {
-    public enum Action {Tidy, Reset, EndTurn, EndGame};
+    public enum Action { Tidy, Reset, EndTurn, EndGame, Undefined };
 
-    private int bowl = 0;
-    private int lastBallScore;
-
-    public static Action NextAction(List<int> pinFalls)
+    public static Action NextAction(List<int> rolls)
     {
-        ActionMaster am = new ActionMaster();
-        Action returnAction = new Action();
+        Action nextAction = Action.Undefined;
 
-        foreach (int pinFall in pinFalls)
+        for (int i = 0; i < rolls.Count; i++)
         {
-             returnAction = am.Bowl(pinFall);
-        }
-        return returnAction;
-    }
-
-	private Action Bowl(int pins)
-    {
-       bowl++;
-
-        // Handles bowl 20, 21, and 19 if 19 is a strike
-        if (bowl >= 20 || (bowl == 19 && pins == 10))
-        {
-            if (lastBallScore + pins < 10 || bowl == 21)
+            if (i == 20)
             {
-                return Action.EndGame;
+                nextAction = Action.EndGame;
             }
-
-            if (lastBallScore == 10 && pins != 10)
+            // Handle last-frame special cases
+            else if (i >= 18 && rolls[i] == 10)
             {
-                return Action.Tidy;
+                nextAction = Action.Reset;
             }
-
-            lastBallScore = pins;
-            return Action.Reset;
+            else if (i == 19)
+            {
+                if (rolls[18] == 10 && rolls[19] == 0)
+                {
+                    nextAction = Action.Tidy;
+                }
+                else if (rolls[18] + rolls[19] == 10)
+                {
+                    nextAction = Action.Reset;
+                }
+                // Roll 21 awarded
+                else if (rolls[18] + rolls[19] >= 10)
+                {
+                    nextAction = Action.Tidy;
+                }
+                else {
+                    nextAction = Action.EndGame;
+                }
+            }
+            // First bowl of frame
+            else if (i % 2 == 0)
+            { 
+                if (rolls[i] == 10)
+                {
+                    // Insert virtual 0 after strike
+                    rolls.Insert(i, 0); 
+                    nextAction = Action.EndTurn;
+                }
+                else {
+                    nextAction = Action.Tidy;
+                }
+            }
+            // Second bowl of frame
+            else
+            { 
+                nextAction = Action.EndTurn;
+            }
         }
 
-        // If first bowl of frame and not a strike
-        if (bowl % 2 != 0 && pins != 10)
-        {
-            lastBallScore = pins;
-            return Action.Tidy;
-        }
-
-        // Second bowl of frame or strike
-        lastBallScore = 0;
-        bowl += (bowl % 2);
-        return Action.EndTurn;    
+        return nextAction;
     }
 }
---
+++ Assets/Scripts/ActionMasterOld.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActionMasterOld
+{
+    public enum Action {Tidy, Reset, EndTurn, EndGame};
+
+    private int bowl = 0;
+    private int lastBallScore;
+
+    public static Action NextAction(List<int> pinFalls)
+    {
+        ActionMasterOld am = new ActionMasterOld();
+        Action returnAction = new Action();
+
+        foreach (int pinFall in pinFalls)
+        {
+             returnAction = am.Bowl(pinFall);
+        }
+        return returnAction;
+    }
+
+	private Action Bowl(int pins)
+    {
+       bowl++;
+
+        // Handles bowl 20, 21, and 19 if 19 is a strike
+        if (bowl >= 20 || (bowl == 19 && pins == 10))
+        {
+            if (lastBallScore + pins < 10 || bowl == 21)
+            {
+                return Action.EndGame;
+            }
+
+            if (lastBallScore == 10 && pins != 10)
+            {
+                return Action.Tidy;
+            }
+
+            lastBallScore = pins;
+            return Action.Reset;
+        }
+
+        // If first bowl of frame and not a strike
+        if (bowl % 2 != 0 && pins != 10)
+        {
+            lastBallScore = pins;
+            return Action.Tidy;
+        }
+
+        // Second bowl of frame or strike
+        lastBallScore = 0;
+        bowl += (bowl % 2);
+        return Action.EndTurn;    
+    }
+}
