86f4ccfb37072d3f525c9da1e6a3cd9922d051ae
Nick Bellamy <nick.bellamy@gmail.com>
2016-02-15 06:45:07 +0000

    Add initialization scores to UI
    These scores are added programtically to check that the script is wired up correctly.

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,18 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ScoreDisplay : MonoBehaviour 
 {
     public Text[] rollsTexts, frameTexts;
 
-	void Start () 
-	{
-	
-	}
-	
-	void Update () 
-	{
-	
-	}
+    void Start()
+    {
+        rollsTexts[0].text = "X";
+        frameTexts[0].text = "0";
+    }
 }
